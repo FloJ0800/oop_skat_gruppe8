141b6296320b9531cd8611483cc9ac4af4ad18c3
Florian Fritz Wiede <florian.wiede@helmholtz-berlin.de>
2024-04-17 09:34:46 +0200

    Sync

--- src/test/java/hwr/oop/ExampleTest.java
+++ src/test/java/hwr/oop/ExampleTest.java
@@ -3,7 +3,6 @@ package hwr.oop;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-// TODO Delete this placeholder test.
 class ExampleTest {
 
   @Test
@@ -15,12 +14,4 @@ class ExampleTest {
     Assertions.assertThat(resultValue).isEqualTo(10);
     Assertions.assertThat(result).isEqualTo(10);
   }
-
-  /*
-  @Test
-  void get_IsTrue() {
-    Example example = new Example();
-    String result = example.get();
-    Assertions.assertThat(result).startsWith("Hello").endsWith("World!");
-  }*/
 }
