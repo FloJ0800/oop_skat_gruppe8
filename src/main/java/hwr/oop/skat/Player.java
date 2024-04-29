f054b5ea7caeef2ab8c6d3e37536d0c6fdee28e0
Katja-Sophie Vogel <katjavogel98@gmail.com>
2024-04-29 12:28:35 +0200

    Added three players

---
+++ src/main/java/hwr/oop/skat/Player.java
@@ -0,0 +1,14 @@
+package hwr.oop.skat;
+
+public class Player {
+    private String name;
+    private boolean alone;
+    private int points = 0;
+
+    public Player(String pName, boolean pAlone) {
+        name = pName;
+        alone = pAlone;
+    }
+}
+
+
--- src/main/java/hwr/oop/skat/SkatGame.java
+++ src/main/java/hwr/oop/skat/SkatGame.java
@@ -4,5 +4,8 @@ class mainOP {
     public static void main(String[] args) {
         CardDeck deck = new CardDeck();
         deck.fillList();
+        Player playerOne = new Player("Katja", true);
+        Player playerTwo = new Player("Florian", false);
+        Player playerThree = new Player("Moritz", false);
     }
 }
