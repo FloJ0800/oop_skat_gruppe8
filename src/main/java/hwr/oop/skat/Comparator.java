1d49912388a3115667daac2ae26a57ab2b53620a
moritz-deutsch <moritz.deutsch@freenet.de>
2024-05-27 10:40:55 +0200

    added Game_logic file and Comparator file

---
+++ src/main/java/hwr/oop/skat/Comparator.java
@@ -0,0 +1,19 @@
+package hwr.oop.skat;
+import hwr.oop.skat.Card;
+
+public class Comparator {
+    
+
+    public static Card getHighestTrick(Card CardA, Card CardB){
+
+        private int comparedBool = Boolean.compare(CardA.isTrump(), CardB.isTrump());
+
+        if (comparedBool == 0){
+
+            
+
+        }
+
+    }
+
+}
